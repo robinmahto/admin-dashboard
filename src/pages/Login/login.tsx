8af50c44454cc06acbcb422c5ea29db6a011c8b6
Robin Mahto <58412832+robinmahto@users.noreply.github.com>
2024-01-01 20:32:35 +0000

    login component added

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,16 +1,61 @@
+import { Button, Card, Checkbox, Form, Input, Layout, Space } from "antd";
+import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons";
 
 const Login = () => {
   return (
     <>
-      <h1>Sign in</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log in</button>
-      <label htmlFor="remember-me">Remember me</label>
-      <input type="checkbox" id="remember-me" />
-      <a href="#">Forgot password</a>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space direction="vertical" align="center" size="large">
+          <Layout.Content>
+            <h1>Pizza</h1>
+          </Layout.Content>
+          <Card
+            bordered={false}
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: "16px",
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+          >
+            <Form>
+              <Form.Item name={"username"}>
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item name={"password"}>
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item name={"remember"}>
+                <Checkbox>Remember me</Checkbox>
+                <a href="#">Forgot Password</a>
+              </Form.Item>
+              <Form.Item name={"password"}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
-  )
-}
+  );
+};
 
 export default Login;
